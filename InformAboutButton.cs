739815a78754a81b09a552936c9a7432dfa0a999
Vyacheslavchik <clavik_2000@mail.ru>
2023-12-04 20:51:04 +0300

    # 1 доделан фуекционал

---
+++ InformAboutButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Тренировка_клавиатурного_тренажера
+{
+    internal class InformAboutButton
+    {
+        public string upValue;
+        public string downValue;
+        public Border border;
+        public string color;
+        public TextBlock textBlock;
+
+        public InformAboutButton(string upValue, string downValue, Border border, string color, TextBlock textBlock)
+        {
+            this.upValue = upValue;
+            this.downValue = downValue;
+            this.border = border;
+            this.color = color;
+            this.textBlock = textBlock;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,11 +20,25 @@ namespace Тренировка_клавиатурного_тренажера
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, InformAboutButton> buttons = new Dictionary<string, InformAboutButton>();
         public MainWindow()
         {
             InitializeComponent();
+            inicalizaidMap();
         }
 
+        private void inicalizaidMap()
+        {
+            foreach(Border border in GridForButtons.Children)
+            {
+                buttons.Add(((TextBlock)border.Child).Text.ToUpper(),
+                    new InformAboutButton( ((TextBlock)border.Child).Text.ToUpper(),
+                                           ((TextBlock)border.Child).Text,
+                                            border,border.Background.ToString(), 
+                                            (TextBlock)border.Child)
+                                          );
+            }
+        }
         private void Start_Button(object sender, RoutedEventArgs e)
         {
             StartButton.IsEnabled = false;
@@ -42,23 +56,36 @@ namespace Тренировка_клавиатурного_тренажера
 
         private void TextBlock_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.E)
-            {
-                ButtonE.Background = new SolidColorBrush(Colors.WhiteSmoke);
-                VvodBlock.Text += ((TextBlock)ButtonE.Child).Text;
+            
+            if (buttons.ContainsKey(e.Key.ToString())) { 
+                buttons[e.Key.ToString()].border.Background = new SolidColorBrush(Colors.WhiteSmoke);
+                VvodBlock.Text += buttons[e.Key.ToString()].textBlock.Text;
             }
-            if(e.Key == Key.CapsLock)
+            if(e.Key == Key.CapsLock || e.Key == Key.RightShift || e.Key == Key.LeftShift)
             {
-                ((TextBlock)ButtonE.Child).Text ="E".Equals(((TextBlock)ButtonE.Child).Text) ? ((TextBlock)ButtonE.Child).Text.ToLower() : ((TextBlock)ButtonE.Child).Text.ToUpper();
+                foreach(string key in buttons.Keys )
+                {
+                    buttons[key].textBlock.Text = buttons[key].textBlock.Text.Equals(buttons[key].upValue) ? buttons[key].downValue : buttons[key].upValue;
+                }
+            }
+            if(e.Key == Key.Back)
+            {
+                VvodBlock.Text = VvodBlock.Text.Substring(0, VvodBlock.Text.Length-1);
             }
         }
         private void TextBlock_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.E)
+            if (buttons.ContainsKey(e.Key.ToString()))
             {
-                ButtonE.Background = new SolidColorBrush(Colors.LightGoldenrodYellow);
+                buttons[e.Key.ToString()].border.Background = (SolidColorBrush)new BrushConverter().ConvertFromString(buttons[e.Key.ToString()].color);
             }
-
+            if (e.Key == Key.RightShift || e.Key == Key.LeftShift)
+            {
+                foreach (string key in buttons.Keys)
+                {
+                    buttons[key].textBlock.Text = buttons[key].textBlock.Text.Equals(buttons[key].upValue) ? buttons[key].downValue : buttons[key].upValue;
+                }
+            }
         }
     }
 }
